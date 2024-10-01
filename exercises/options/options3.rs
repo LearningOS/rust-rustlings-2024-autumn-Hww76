4d82380517f56bfa457bb1bbec6e45785daaeb16
Hww76 <2941697089@qq.com>
2024-10-01 12:25:55 +0800

    49-51:have done

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -3,8 +3,7 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
+// 这题考ref的使用，在第17行Some(p) 改为Some(ref p)，这样y仍然保留所有权
 struct Point {
     x: i32,
     y: i32,
@@ -13,8 +12,8 @@ struct Point {
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
-    match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+    match y { 
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
     y; // Fix without deleting this line.
